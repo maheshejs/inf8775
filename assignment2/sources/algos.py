4f028fcb9f3172e69f99b2040afb6241cd7f1774
Joseph Maheshe <maheshejs@gmail.com>
2022-03-13 01:21:48 -0500

    ASS2 : Fix update method of candidate and add custom bisect method

--- assignment2/sources/algos.py
+++ assignment2/sources/algos.py
@@ -1,8 +1,18 @@
 from abc import ABC, abstractmethod
 from bisect import bisect
-from typing import List
+from typing import List, Callable
 from collections import deque
-from bisect import bisect_left
+
+def bisect(lst: List[List[int]], value: List[int], key: Callable[[List[int]], int]):
+    lo = 0
+    hi = len(lst)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if key(lst[mid]) > key(value):
+            lo = mid + 1
+        else:
+            hi = mid
+    return lo
 
 # Interface for algorithm
 class IAlgo(ABC) :
@@ -55,19 +65,25 @@ class Candidate:
         return sum(heights)
     
     @property
+    def blocks(self) -> List[List[int]] :
+        return self._blocks
+    
+    @property
     def tabu(self) -> List[List[int]] :
         return self._tabu
     
     def update(self, block: List[int]) -> None:
-        idxs  = [bisect_left(self._blocks, block, key = lambda x : x[1]),
-                  bisect_left(self._blocks, block, key = lambda x : x[2])]
-        idx   = max(idxs)
+        idxs  = [bisect(self._blocks, block, key = lambda x: x[1]),
+                  bisect(self._blocks, block, key = lambda x: x[2])]
+        idx   = min(idxs)
         upper = deque(self._blocks[idx:])
         self._blocks = self._blocks[:idx]
         self._blocks.append(block)
         self._tabu = []
         while upper :
-            if upper[0][1] >= block[1] or upper[0][2] >= block[2] :
+            if upper[0][1] < block[1] and upper[0][2] < block[2] :
+              break
+            else :
                 self._tabu.append(upper[0])
                 upper.popleft()
         self._blocks.extend(upper)
@@ -76,3 +92,19 @@ class Candidate:
 class TabuAlgo(IAlgo):
     def solve(self, blocks: List[List[int]]) -> List[List[int]] :
         return []
+
+
+if __name__ == "__main__" :
+    blocks = [[10, 5, 8],[5, 8, 10],[8, 5, 10],[7, 1, 15],
+              [15, 1, 7],[1, 7, 15],[4, 9, 14],[9, 4, 14],
+              [14, 4, 9],[13, 2, 3],[3, 2, 13],[2, 3, 13],
+              [12, 6, 11],[11, 6, 12],[6, 11, 12]]
+  
+    candidate = Candidate(blocks)
+    for block in candidate.blocks :
+        print(*block)
+    print("Height : ", candidate.height)
+    candidate.update([21, 7, 11])
+    for block in candidate.blocks :
+        print(*block)
+    print("Height : ", candidate.height)
